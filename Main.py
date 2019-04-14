99ab84af3da6662ff290ae62f1a5fb25600c6c00
omarh <omar.hawari.97@hotmail.com>
2019-04-14 17:21:58 +0300

    Second commit - Omar's part

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ from sub_systems.AsthmaAttack import *
 from sub_systems.BlueRingedBites import *
 from sub_systems.MarineStings import *
 from sub_systems.NonTropicalJellyfishStings import *
+from sub_systems.Poisoning import *
+from sub_systems.RedBackSpiderBite import *
 from sub_systems.Burns import *
 from Data import *
 
@@ -13,6 +15,8 @@ CASE_DRSABCD = "DRSABCD"
 CASE_INSECT_BITE = "Insect Bite"
 CASE_MARINE_STING = "Marine Sting"
 CASE_NONE_TROPICAL_JELLYFISH_STINGS = "Non-Tropical Jellyfish Stings"
+CASE_POISONING = "Poisoning"
+CASE_RED_BACK_SPIDER_BITE = "Red-Back Spider Bite"
 ASTHMA_ATTACK = "Asthma Attack"
 BLUE_RINGED_BITES = "Blue Ringed Bites"
 BURNS = "Burns and Scalds"
@@ -56,12 +60,26 @@ class MainEngine(KnowledgeEngine):
         engine.onComplete = onComplete
         engine.startEngine()
 
+    @Rule(Case("5"))
+    def startInsectBite(self):
+        engine = Poisoning()
+        engine.onComplete = onComplete
+        engine.startEngine()
+
+    @Rule(Case("6"))
+    def startInsectBite(self):
+        engine = RedBackSpiderBite()
+        engine.onComplete = onComplete
+        engine.startEngine()
+
     def startEngine(self):
         self.reset()
         self.declare(Case(input(f"Choose case: (1- {CASE_DRSABCD},"
                                 f" 2- {CASE_INSECT_BITE},"
                                 f" 3- {CASE_MARINE_STING},"
-                                f" 4- {CASE_NONE_TROPICAL_JELLYFISH_STINGS})\n")))
+                                f" 4- {CASE_NONE_TROPICAL_JELLYFISH_STINGS},"
+                                f" 5- {CASE_POISONING},"
+                                f" 6- {CASE_RED_BACK_SPIDER_BITE})\n")))
         self.run()
 
     @Rule(NOT(Case(CASE_DRSABCD) & Case(CASE_INSECT_BITE)))
--- sub_systems/Poisoning.py
+++ sub_systems/Poisoning.py
@@ -24,6 +24,49 @@ class Poisoning(KnowledgeEngine):
         self.determineCasualtyCase()
 
     def determineCasualtyCase(self):
-        pass
+        self.reset()
+        self.declare(Case(input('What\'s the case?\n'
+                                '1- Conscious casualty.\n'
+                                '2- Injected.\n'
+                                '3- Unconscious casualty.\n'
+                                '4- Absorbed poisons.\n'
+                                '5- Inhaled Poisons.\n')))
+        self.run()
+
+    @Rule(Case('1'))
+    def conscious(self):
+        instructions.append('Listen to the casualty and give reassurance.')
+        instructions.append('Find out what sort of poison is involved and keep any containers for the medical aid to '
+                            'see.')
+        instructions.append('DO NOT induce vomiting or give anything to eat or drink.')
+        instructions.append('Wash any corrosive substance off the mouth and face with water, or wipe off.')
+        self.onComplete()
+
+    @Rule(Case('2'))
+    def unconscious(self):
+        instructions.append('Place the casualty in the recovery position and continue to check their airway and '
+                            'breathing regularly.')
+        self.onComplete()
+
+    @Rule(Case('3'))
+    def inhaled(self):
+        instructions.append('Move casualty and yourself to fresh air.')
+        instructions.append('Loosen tight clothing.')
+        self.onComplete()
+
+    @Rule(Case('4'))
+    def absorbedPoisons(self):
+        instructions.append('Protect yourself (if possible) use protective clothing such as gloves, goggles and so on.')
+        instructions.append('Wash the substance off immediately.')
+        instructions.append('Ask the casualty to remove any contaminated clothing.')
+        instructions.append('Flush the casualty’s skin with running water.')
+        self.onComplete()
+
+    @Rule(Case('5'))
+    def injected(self):
+        instructions.append('avoid needle stick injuries to yourself and casualty.')
+        instructions.append('Treat any other signs and symptoms. Send any empty syringes, bottles and vials with the '
+                            'casualty to hospital. ')
+        self.onComplete()
 
     pass
